e7cc7b9e61aa34c76e0976323ef26a25648ae0c9
john-desing <john-desing@users.noreply.github.com>
2019-11-26 12:29:49 +0100

    Update main.c
    Ajout multiplication

--- main.c
+++ main.c
@@ -5,10 +5,15 @@ int addition (int a, int b)
 {
      return a+b;
 }
-int multiplication (int a )
+int multiplication (int a , int b )
 
 int main()
 {
     printf("L’addition de a et de b vaut %d\n",addition(1,4));
+    return 0;
+}
+int main()
+{
+    printf("Le produit de a et 2 vaux  %d\n",multiplication(3,5));
     return 0;
 }
